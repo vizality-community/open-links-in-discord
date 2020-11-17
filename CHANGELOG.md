61bb0557b6e3e4b426b6e8204e54a896f8c5e15f
vizality <contact@vizality.com>
2020-11-16 23:28:49 -0500

    Submit

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. This project attempts to loosely adhere to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.0.2] - 2020-11-16
+
+- Ported over to Vizality by dperolio
+- Reduced code requirements by utilizing Vizality's API
+
+## [0.0.1] - 2020-11-14
+
+- Initial release by AAGaming for Powercord
---
+++ index.jsx
@@ -0,0 +1,32 @@
+const { patch, unpatch } = require('@vizality/patcher');
+const { getModule } = require('@vizality/webpack');
+const { Plugin } = require('@vizality/entities');
+
+module.exports = class OpenLinksInDiscord extends Plugin {
+  onStart () {
+    const Anchor = getModule(m => m.default && m.default.displayName === 'Anchor');
+
+    patch('open-links-in-discord', Anchor, 'default', (_, res) => {
+      res.props.oClick = res.props.onClick;
+      res.props.onClick = (e) => {
+        if (!e.shiftKey) {
+          res.props.oClick(e);
+          return;
+        }
+
+        e.preventDefault();
+
+        const url = res.props.href;
+        if (url) vizality.api.popup.openWindow({ url });
+
+        return false;
+      };
+
+      return res;
+    });
+  }
+
+  onStop () {
+    unpatch('open-links-in-discord');
+  }
+};
