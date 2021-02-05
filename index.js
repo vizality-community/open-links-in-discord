74f4cdad5101ba151c2c3f650978b33d288e9413
dperolio <dustin.perolio@outlook.com>
2021-02-05 04:34:37 -0500

    Change icon, banner, and do some code cleanup

--- index.js
+++ index.js
@@ -2,29 +2,18 @@ import { patch, unpatch } from '@vizality/patcher';
 import { getModule } from '@vizality/webpack';
 import { Plugin } from '@vizality/entities';
 
-export default class OpenLinksInDiscord extends Plugin {
+export default class extends Plugin {
   start () {
     const Anchor = getModule(m => m.default?.displayName === 'Anchor');
-
     patch('open-links-in-discord', Anchor, 'default', (_, res) => {
-      res.props.oClick = res.props.onClick;
+      const ogOnClick = res.props.onClick;
       res.props.onClick = e => {
-        if (!e.shiftKey) {
-          res.props.oClick(e);
-          return;
-        }
-
+        if (!e.shiftKey) return ogOnClick(e);
         e.preventDefault();
-
         const url = res.props.href;
-
-        if (url) {
-          vizality.api.windows.openWindow({ url });
-        }
-
+        if (url) vizality.api.popups.openPopup({ url });
         return false;
       };
-
       return res;
     });
   }
